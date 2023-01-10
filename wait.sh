fecd493cc3e15447dc4dc96b10a447ff0929b6ba
Mazen Mohamed <mazen.mohamed.967@gmail.com>
2023-01-10 05:25:08 +0200

    modify docker web app entry point script

--- wait.sh
+++ wait.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-sleep 5
+until mysql -u root -e ""
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
 
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
